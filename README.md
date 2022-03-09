6a628fe936eac8a2d9b000fa809367021a81c903
MaxBauV <max-bauer@eclipso.at>
2022-03-09 16:15:49 +0100

    New code - changed to UDP protocol

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # FH_SAE
 
-Source: https://www.geeksforgeeks.org/socket-programming-cc/
+Source: 
+https://www.geeksforgeeks.org/udp-server-client-implementation-c/
+
+https://www.geeksforgeeks.org/socket-programming-cc/
--- server.c
+++ server.c
@@ -1,59 +1,54 @@
-// Server side C/C++ program to demonstrate Socket programming
-#include <unistd.h>
+// Server side implementation of UDP client-server model
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
-#include <netinet/in.h>
+#include <unistd.h>
 #include <string.h>
-#define PORT 8080
-int main(int argc, char const *argv[])
-{
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[1024] = {0};
-	char *hello = "Hello from server";
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT	8080
+#define MAXLINE 1
+
+// Driver code
+int main() {
+	int sockfd;
+	char buffer[MAXLINE];
+	char *msg = "s";
+	struct sockaddr_in servaddr, cliaddr;
 	
 	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed");
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
 	
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-												&opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
 	
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address,
-								sizeof(address))<0)
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+	
+	// Bind the socket with the server address
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
 	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-	if (listen(server_fd, 3) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-					(socklen_t*)&addrlen))<0)
-	{
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	valread = read( new_socket , buffer, 1024);
-	printf("%s\n",buffer );
-	send(new_socket , hello , strlen(hello) , 0 );
-	printf("Hello message sent\n");
+	
+	socklen_t len = sizeof(cliaddr); //len is value/resuslt
+    int n = 0;
+
+    for (int i = 0; i < 50000; i++)
+    {
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+        printf("Client %d: %s\n", i, buffer);
+        sendto(sockfd, (const char *)msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+    }
+	
 	return 0;
 }
