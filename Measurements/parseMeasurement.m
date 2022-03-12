d45ceaafeb12533af743b8f6dbc774119a1cf632
Johannes <yoshie1997@gmail.com>
2022-03-12 19:36:37 +0100

    Update parseMeasurement.m

--- Measurements/parseMeasurement.m
+++ Measurements/parseMeasurement.m
@@ -1,20 +1,26 @@
 close
 clear
 
+%Variables
 above = 0;
 below = 0;
 
-filename = uigetfile(pwd());
+%load file, remove text in first row
+filename = uigetfile();
 data = csvread(filename);
 data = data(2:end);
+
+%Get Min, Max, Mean
 Minimum = min(data)
 Maximum = max(data)
 Mean = mean(data)
 
+%Let user pick upper and lower bounds
 hist(data, 1000);
 upper = input("Pick upper bound: ")
 lower = input("Pick lower bound: ")
 
+%Values that are out of bounds will be set to boundary value
 for i = 1:length(data)
   if data(i,1) > upper;
     data(i, 1) = upper;
@@ -28,9 +34,13 @@ for i = 1:length(data)
     below++;
   endif
 endfor
+
+%Get data for histogram with 10us bins
 bins = lower:10:upper;
 plots = lower+5:10:upper+5;
 dist = histc(data, bins);
+
+%Plot resulting histogram
 bar(plots, dist);
 grid on;
 grid minor;
